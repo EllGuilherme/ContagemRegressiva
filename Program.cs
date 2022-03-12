0c354cf76235d55b15ae8b5bb14ba39894ddb790
Ellguilherme <grds1000@gmail.com>
2022-03-12 01:13:29 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine(" Preparar...!");
+Console.ResetColor();
+Console.ReadKey();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(" Apontar...!");
+Console.ReadKey();
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(" FOGO!!"); 
+Console.WriteLine();
+Console.ResetColor();
